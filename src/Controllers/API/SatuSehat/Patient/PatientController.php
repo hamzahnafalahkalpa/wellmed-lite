fa5688f23a44daa339258e80019ae0428211b341
hamzah <hamzahnafalah@gmail.com>
2025-10-15 16:38:25 +0700

    update satu sehat

---
+++ src/Controllers/API/SatuSehat/Patient/PatientController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Projects\WellmedLite\Controllers\API\SatuSehat\Patient;
+
+use Hanafalah\SatuSehat\Contracts\Schemas\Fhir\Patient\PatientSatuSehat;
+use Illuminate\Http\Request;
+use Projects\WellmedLite\Controllers\API\ApiController;
+
+class PatientController extends ApiController{
+    public function __construct(
+        protected PatientSatuSehat $__patient_schema
+    )
+    {
+        parent::__construct();
+    }
+
+    public function store(Request $request){
+        request()->replace([
+            'form' => request()->all()
+        ]);
+        return $this->__patient_schema->storePatientSatuSehat();
+    }
+}
