8e5075efb3d14d9e42577c5a4c8143e90734a45b
hamzah <hamzahnafalah@gmail.com>
2025-10-16 17:16:27 +0700

    update satu sehat

--- src/Providers/WellmedLiteServiceProvider.php
+++ src/Providers/WellmedLiteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Projects\WellmedLite\Providers;
 
+use Hanafalah\ApiHelper\Facades\ApiAccess;
 use Illuminate\Foundation\Http\Kernel;
 use Hanafalah\LaravelSupport\{
     Concerns\NowYouSeeMe,
@@ -16,6 +17,8 @@ use Projects\WellmedLite\{
 use Hanafalah\LaravelSupport\Middlewares\PayloadMonitoring;
 use Hanafalah\MicroTenant\Contracts\Supports\ConnectionManager;
 use Hanafalah\MicroTenant\Facades\MicroTenant;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Schema;
 use Projects\WellmedLite\Supports\ConnectionManager as SupportsConnectionManager;
 
@@ -48,35 +51,49 @@ class WellmedLiteServiceProvider extends WellmedLiteEnvironment
 
     public function boot(Kernel $kernel){        
         $kernel->pushMiddleware(PayloadMonitoring::class);
-        try {
-            $tenant = $this->TenantModel()->where('flag','APP')->where('props->product_type','WELLMED_LITE')->first();
-            if (isset($tenant)){
-                $model  = Facades\WellmedLite::myModel($tenant);
-                $this->deferredProviders($model);
+        $this->app->booted(function(){
+            try {
+                $tenant = $this->TenantModel()->where('flag','APP')->where('props->product_type','WELLMED_LITE')->first();
+                if (isset($tenant)){
+                    MicroTenant::tenantImpersonate($tenant);
+                    // Event::listen(\Laravel\Octane\Events\RequestReceived::class, function ($event) use ($tenant) {
+                    //     MicroTenant::tenantImpersonate($tenant);
+                    //     ApiAccess::init()->accessOnLogin(function ($api_access) {
+                    //         Auth::setUser($api_access->getUser());
+                    //     //     // tenancy()->end();
+                    //     //     // tenancy()->initialize($microtenant?->tenant->model ?? $microtenant?->group->model ?? $microtenant?->project->model);
+                    //     });
+                    //     // tenancy()->end();
+                    //     // tenancy()->initialize($tenant);
+                    // });
     
-                $this->registers([
-                    '*', 
-                    'Provider' => function() use ($model){
-                        $this->bootedRegisters($model->packages, 'wellmed-lite', __DIR__.'/../'.$this->__config_wellmed_lite['libs']['migration'] ?? 'Migrations');
-                        $this->registerOverideConfig('wellmed-lite',__DIR__.'/../'.$this->__config_wellmed_lite['libs']['config']);
-                    },
-                    'Model', 'Database'
-                ]);
-                $this->autoBinds();
-                tenancy()->end();
-                tenancy()->initialize($tenant);
-                $this->registerRouteService(RouteServiceProvider::class);
-    
-                $this->app->singleton(PathRegistry::class, function() use ($tenant) {
-                    $registry = new PathRegistry();
-    
-                    $config = config("wellmed-lite");
-                    foreach ($config['libs'] as $key => $lib) $registry->set($key, 'projects'.$lib);
-                    
-                    return $registry;
-                });
+                    $model  = Facades\WellmedLite::myModel($tenant);
+                    $this->deferredProviders($model);
+        
+                    $this->registers([
+                        '*', 
+                        'Provider' => function() use ($model){
+                            $this->bootedRegisters($model->packages, 'wellmed-lite', __DIR__.'/../'.$this->__config_wellmed_lite['libs']['migration'] ?? 'Migrations');
+                            $this->registerOverideConfig('wellmed-lite',__DIR__.'/../'.$this->__config_wellmed_lite['libs']['config']);
+                        },
+                        'Model', 'Database'
+                    ]);
+                    $this->autoBinds();
+                    // tenancy()->end();
+                    // tenancy()->initialize($tenant);
+                    $this->registerRouteService(RouteServiceProvider::class);
+        
+                    $this->app->singleton(PathRegistry::class, function() use ($tenant) {
+                        $registry = new PathRegistry();
+        
+                        $config = config("wellmed-lite");
+                        foreach ($config['libs'] as $key => $lib) $registry->set($key, 'projects'.$lib);
+                        
+                        return $registry;
+                    });
+                }
+            } catch (\Throwable $th) {
             }
-        } catch (\Throwable $th) {
-        }
+        });
     }    
 }
