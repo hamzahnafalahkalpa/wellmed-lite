cddbd33b9f1c0098de902a547684b9ec0f9f4557
hamzah <hamzahnafalah@gmail.com>
2025-10-23 13:54:39 +0700

    update for s3

---
+++ src/Controllers/API/SatuSehat/Organization/OrganizationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Projects\WellmedLite\Controllers\API\SatuSehat\Organization;
+
+use Hanafalah\SatuSehat\Contracts\Schemas\Fhir\Organization\OrganizationSatuSehat;
+use Illuminate\Http\Request;
+use Projects\WellmedLite\Controllers\API\ApiController;
+
+class OrganizationController extends ApiController{
+    public function __construct(
+        protected OrganizationSatuSehat $__organization_schema
+    ){
+        parent::__construct();
+    }
+
+    public function store(Request $request){
+        request()->replace([
+            'form' => request()->all()
+        ]);
+        return $this->__organization_schema->storeOrganizationSatuSehat();
+    }
+
+    public function update(Request $request){
+        return $this->__organization_schema->updateOrganizationSatuSehat();
+    }
+}
